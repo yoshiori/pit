5a1cecbd1609d85222d4533db6d6ffedb01ded08
yoshiori <yoshiori@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-20 09:17:12 +0000

    とりあえず動くかもレベルで完成
    git-svn-id: http://svn.coderepos.org/share/lang/python/pit@14293 d0d07461-0603-4401-acd4-de1884942a52

--- pit.py
+++ pit.py
@@ -1,60 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os, yaml
+import os, yaml, tempfile
+from subprocess import Popen
 
 class Pit:
-    Directory = os.path.expanduser('~/.pitPy')
-    __config = os.path.join(Directory, 'pit.yaml')
-    __profile = os.path.join(Directory, 'default.yaml')
+    VERSION   = "0.0.1"
+    DIRECTORY = os.path.expanduser('~/.pit')
+    __config = os.path.join(DIRECTORY, 'pit.yaml')
+    __profile = os.path.join(DIRECTORY, 'default.yaml')
+    
+    @staticmethod
+    def set(name, opts={}):
+        profile = Pit.__load()
+        ret = {}
+        if opts.has_key('data'):
+            ret = opts['data']
+        else:
+            if not os.environ.has_key('EDITOR'):
+                return {}
+            t = tempfile.NamedTemporaryFile()
+            c = yaml.dump(opts['config'] if opts.has_key('config') else Pit.get(name) ,default_flow_style=False)
+            t.write(c)
+            t.flush()
+            path = os.path.abspath(t.name)
+            Popen([os.environ['EDITOR'],path]).communicate()
+            result = open(path).read()
+            if result == c:
+                print 'No Changes'
+            result = yaml.load(result)
+            profile[name] = result
+            yaml.dump(profile,
+                      open(Pit.__profile, 'w'),
+                      default_flow_style=False)
+            return result
 
-    def set(self, name, opts={}):
-        profile = self.__load()
-#       t = os.tmpfile()
+    @staticmethod
+    def get(name, opts={}):
+        load_data = Pit.__load()
+        ret = load_data[name] if load_data.has_key(name) else {} 
+        if opts.has_key('require'):
+            for k, v in opts['require'].iteritems():
+                ret[k] = v
+            ret = Pit.set(name,{'config' : ret})
+        return ret or {'username' : '', 'password' : ''}
 
-    def get(self, name, opts={}):
-        pass
-
-    def switch(self, name, opts={}):
-        self.__profile = os.path.join(self.Directory, '%s.yaml' % name)
-        config = self.config()
+    @staticmethod
+    def switch(name, opts={}):
+        Pit.__profile = os.path.join(Pit.DIRECTORY, '%s.yaml' % name)
+        config = Pit.config()
         ret = config['profile']
         config['profile'] = name
-        print config
         yaml.dump(config,
-                  open(self.__config, 'w'),
+                  open(Pit.__config, 'w'),
                   default_flow_style=False)
-        print 'func switch :' + name
         return ret
 
-    def __load(self):
-        if not os.path.exists(self.Directory):
-            os.mkdir(self.Directory)
-            os.chmod(self.Directory, 0700)
+    @staticmethod
+    def __load():
+        if not os.path.exists(Pit.DIRECTORY):
+            os.mkdir(Pit.DIRECTORY)
+            os.chmod(Pit.DIRECTORY, 0700)
 
-        if not os.path.exists(self.__config):
+        if not os.path.exists(Pit.__config):
             yaml.dump({'profile' : 'default'},
-                      open(self.__config, 'w'),
+                      open(Pit.__config, 'w'),
                       default_flow_style=False)
-            os.chmod(self.__config, 0600)
+            os.chmod(Pit.__config, 0600)
 
-        self.switch(self.config()['profile'])
+        Pit.switch(Pit.config()['profile'])
 
-        if not os.path.exists(self.__profile):
+        if not os.path.exists(Pit.__profile):
             yaml.dump({}, 
-                      open(self.__profile, 'w'), 
+                      open(Pit.__profile, 'w'), 
                       default_flow_style=False)
-            os.chmod(self.__profile, 0600)
+            os.chmod(Pit.__profile, 0600)
+        return yaml.load(open(Pit.__profile)) or {}
 
-        return yaml.load(open(self.__profile)) or {}
-
-    def config(self):
-        return yaml.load(open(self.__config))
+    @staticmethod
+    def config():
+        return yaml.load(open(Pit.__config))
 
 if __name__ == '__main__':
-    Pit().set('vox.com')
-    
-'''
-switch の opts は何につかうの?
-
-
-'''
+    config = Pit.get('twitter.com',{'require': {'email':'your email','password':'your password'}})
+    print config['email']
+    print config['password']
