5842cb73b91d184c9d22aa1764f477877db5a20c
yoshiori <yoshiori@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-25 16:41:44 +0000

    delete class
    git-svn-id: http://svn.coderepos.org/share/lang/python/pit@22127 d0d07461-0603-4401-acd4-de1884942a52

--- pit_oneliner.py
+++ pit_oneliner.py
@@ -6,93 +6,89 @@ from subprocess import Popen
 g = globals()
 g.__setitem__("setitem", g.__setitem__)
 
-class Pit:
-    VERSION   = "0.0.1"
-    DIRECTORY = os.path.expanduser('~/.pit')
-    _config = os.path.join(DIRECTORY, 'pit.yaml')
-    _profile = os.path.join(DIRECTORY, 'default.yaml')
+
+setitem('VERSION', "0.0.1")
+setitem('DIRECTORY', os.path.expanduser('~/.pit'))
+setitem('_config', os.path.join(DIRECTORY, 'pit.yaml'))
+setitem('_profile', os.path.join(DIRECTORY, 'default.yaml'))
     
-    @staticmethod
-    def set(name, opts={}):
-        setitem('set_profile', Pit._load())
-        setitem('set_result', {})
-        if opts.has_key('data'):
-            setitem('set_result', opts['data'])
-        else:
-            if not os.environ.has_key('EDITOR'):
-                return {}
-            setitem('set_if_t', tempfile.NamedTemporaryFile())
-            setitem('set_if_c', yaml.dump(opts['config'] if opts.has_key('config') else Pit.get(name) ,default_flow_style=False))
-            set_if_t.write(c)
-            set_if_t.flush()
-            setitem('set_if_path', os.path.abspath(t.name))
-            Popen([os.environ['EDITOR'],path]).communicate()
-            setitem('set_result', open(path).read())
-            if set_result == c:
-                print 'No Changes'
-                return set_profile[name]
 
-            setitem('set_result', yaml.load(result))
+def set(name, opts={}):
+    setitem('set_profile', _load())
+    setitem('set_result', {})
+    if opts.has_key('data'):
+        setitem('set_result', opts['data'])
+    else:
+        if not os.environ.has_key('EDITOR'):
+            return {}
+        setitem('set_if_t', tempfile.NamedTemporaryFile())
+        setitem('set_if_c', yaml.dump(opts['config'] if opts.has_key('config') else get(name) ,default_flow_style=False))
+        set_if_t.write(c)
+        set_if_t.flush()
+        setitem('set_if_path', os.path.abspath(t.name))
+        Popen([os.environ['EDITOR'],path]).communicate()
+        setitem('set_result', open(path).read())
+        if set_result == c:
+            print 'No Changes'
+            return set_profile[name]
+        
+        setitem('set_result', yaml.load(result))
 
-        setitem('set_profile_'+ name, set_result)
-        yaml.dump(set_profile,
-                  open(_profile, 'w'),
-                  default_flow_style=False)
-        return set_result
+    setitem('set_profile_'+ name, set_result)
+    yaml.dump(set_profile,
+              open(_profile, 'w'),
+              default_flow_style=False)
+    return set_result
 
-    @staticmethod
-    def get(name, opts={}):
-        setitem('load_data', Pit._load())
-        setitem('get_ret', load_data[name] if load_data.has_key(name) else {} )
-        if opts.has_key('require'):
-            setitem('get_for_keys', set(opts['require'].keys()) - set(get_ret.keys()))
-            if get_for_keys:
-                for key in keys:
-                    setitem('get_for_ret_' + key,opts['require'][key]) 
-                setitem('get_ret', Pit.set(name,{'config' : get_ret}))
+def get(name, opts={}):
+    setitem('load_data', _load())
+    setitem('get_ret', load_data[name] if load_data.has_key(name) else {} )
+    if opts.has_key('require'):
+        setitem('get_for_keys', set(opts['require'].keys()) - set(get_ret.keys()))
+        if get_for_keys:
+            for key in keys:
+                setitem('get_for_ret_' + key,opts['require'][key]) 
+            setitem('get_ret', set(name,{'config' : get_ret}))
           
-        return get_ret or {'username' : '', 'password' : ''}
+    return get_ret or {'username' : '', 'password' : ''}
 
-    @staticmethod
-    def switch(name, opts={}):
-        setitem('_profile', os.path.join(Pit.DIRECTORY, '%s.yaml' % name))
-        setitem('switch_config', Pit.config())
-        setitem('switch_ret', switch_config['profile'])
-        setitem('switch_config_profie',name)
-        yaml.dump(switch_config,
-                  open(Pit._config, 'w'),
+def switch(name, opts={}):
+    setitem('_profile', os.path.join(DIRECTORY, '%s.yaml' % name))
+    setitem('switch_config', config())
+    setitem('switch_ret', switch_config['profile'])
+    setitem('switch_config_profie',name)
+    yaml.dump(switch_config,
+              open(_config, 'w'),
+              default_flow_style=False)
+    return switch_ret
+
+def _load():
+    if not os.path.exists(DIRECTORY):
+        os.mkdir(DIRECTORY)
+        os.chmod(DIRECTORY, 0700)
+
+    if not os.path.exists(_config):
+        yaml.dump({'profile' : 'default'},
+                  open(_config, 'w'),
                   default_flow_style=False)
-        return switch_ret
+        os.chmod(_config, 0600)
 
-    @staticmethod
-    def _load():
-        if not os.path.exists(Pit.DIRECTORY):
-            os.mkdir(Pit.DIRECTORY)
-            os.chmod(Pit.DIRECTORY, 0700)
+    switch(config()['profile'])
+    
+    if not os.path.exists(_profile):
+        yaml.dump({}, 
+                  open(_profile, 'w'), 
+                  default_flow_style=False)
+        os.chmod(_profile, 0600)
+    return yaml.load(open(_profile)) or {}
 
-        if not os.path.exists(Pit._config):
-            yaml.dump({'profile' : 'default'},
-                      open(Pit._config, 'w'),
-                      default_flow_style=False)
-            os.chmod(Pit._config, 0600)
-
-        Pit.switch(Pit.config()['profile'])
-
-        if not os.path.exists(Pit._profile):
-            yaml.dump({}, 
-                      open(Pit._profile, 'w'), 
-                      default_flow_style=False)
-            os.chmod(Pit._profile, 0600)
-        return yaml.load(open(Pit._profile)) or {}
-
-    @staticmethod
-    def config():
-        return yaml.load(open(Pit._config))
+def config():
+    return yaml.load(open(_config))
 
 if __name__ == '__main__':
-    config = Pit.get('34twitter.com',{'require': {'email':'your email','password':'your password'}})
-    print config
-    print config['email']
-    print config['password']
+    setitem('__main__config', get('twitter.com',{'require': {'email':'your email','password':'your password'}}))
+    print __main__config
+    print __main__config['email']
+    print __main__config['password']
 
 
--- test_oneloner.py
+++ test_oneloner.py
@@ -1,20 +1,19 @@
-import unittest
-from pit_oneliner import Pit
+import unittest, pit_oneliner
 
 class PitTestCase(unittest.TestCase):
     def test_set_get(self):
-        Pit.set("test", {'data':{'username':'foo','password':'bar'}})
-        self.assertEqual("foo", Pit.get("test")["username"] )
-        self.assertEqual("bar", Pit.get("test")["password"] )
+        pit_oneliner.set("test", {'data':{'username':'foo','password':'bar'}})
+        self.assertEqual("foo", pit_oneliner.get("test")["username"] )
+        self.assertEqual("bar", pit_oneliner.get("test")["password"] )
 
-        Pit.set("test2", {'data' : {"username":"foo2","password":"bar2"}})
-        self.assertEqual( "foo2", Pit.get("test2")["username"])
-        self.assertEqual( "bar2", Pit.get("test2")["password"])
+        pit_oneliner.set("test2", {'data' : {"username":"foo2","password":"bar2"}})
+        self.assertEqual( "foo2", pit_oneliner.get("test2")["username"])
+        self.assertEqual( "bar2", pit_oneliner.get("test2")["password"])
         
-        Pit.set("test", {'data' : {"username":"foo","password":"bar"}})
-        self.assertEqual( "foo", Pit.get("test")["username"])
-        self.assertEqual( "bar", Pit.get("test")["password"])
+        pit_oneliner.set("test", {'data' : {"username":"foo","password":"bar"}})
+        self.assertEqual( "foo", pit_oneliner.get("test")["username"])
+        self.assertEqual( "bar", pit_oneliner.get("test")["password"])
 
-        self.assertEqual( "foo", Pit.set("test",{'data':{"username":"foo","password":"bar"}})["username"])
+        self.assertEqual( "foo", pit_oneliner.set("test",{'data':{"username":"foo","password":"bar"}})["username"])
 if __name__ == "__main__":
         unittest.main()
