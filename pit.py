eaaab19797aa39dc814ceb0d91c8bb3a52e699de
yoshiori <yoshiori@d0d07461-0603-4401-acd4-de1884942a52>
2010-05-06 10:19:03 +0000

    change ver
    git-svn-id: http://svn.coderepos.org/share/lang/python/pit@37391 d0d07461-0603-4401-acd4-de1884942a52

--- pit.py
+++ pit.py
@@ -4,7 +4,7 @@ import os, yaml, tempfile
 from subprocess import Popen
 
 class Pit:
-    VERSION   = "0.0.1"
+    VERSION   = "0.3"
     DIRECTORY = os.path.expanduser('~/.pit')
     _config = os.path.join(DIRECTORY, 'pit.yaml')
     _profile = os.path.join(DIRECTORY, 'default.yaml')
